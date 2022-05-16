a125daac3dd1ace98f301cf45dc05c4f191fdad4
LAPTOP-4T7CNI7E\LeoNgan <leo.ngan@autotoll.com.hk>
2022-05-16 17:30:13 +0800

    finish nav bar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,4 +1,13 @@
-import { SearchOffOutlined } from "@mui/icons-material";
+import {
+  DarkModeOutlined,
+  LanguageOutlined,
+  LightModeOutlined,
+  SearchOffOutlined,
+  FullscreenExitOutlined,
+  NotificationsNoneOutlined,
+  ChatBubbleOutlineOutlined,
+  ListOutlined,
+} from "@mui/icons-material";
 import React from "react";
 import "./NavBar.scss";
 
@@ -7,11 +16,41 @@ const NavBar = () => {
     <div className="navbar">
       <div className="wrapper">
         <div className="search">
-          <input type="text" name="" id="" placeholder="search" />
+          <input type="text" name="" id="" placeholder="Search" />
           <SearchOffOutlined />
         </div>
 
-        .
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            <div className="langrage">English</div>
+          </div>
+          <div className="item">
+            <DarkModeOutlined className="icon" />
+            {/* <LightModeOutlined /> */}
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/7015865/pexels-photo-7015865.jpeg?cs=srgb&dl=pexels-alina-vilchenko-7015865.jpg&fm=jpg"
+              alt="Photo by Alina Vilchenko from Pexels"
+              className="avatar"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
